a27042083045c19b32c2ab29e4c1d0c8c47916b2
Naimur53 <naimurrhman53@gmail.com>
2022-10-03 00:42:29 +0600

    don

--- components/HomeBanner/HomeBanner.js
+++ components/HomeBanner/HomeBanner.js
@@ -4,12 +4,12 @@ const HomeBanner = () => {
     return (
         <div className="mt-[100px] md:mt-[152px]">
             <div className="">
-                <div className="grid grid-cols-1 md:grid-cols-7 gap-4 ">
+                <div className="grid grid-cols-1 md:grid-cols-7 md:gap-4 ">
                     <div className="col-span-4">
-                        <h1 className="font-extrabold text-xl mb-[16px]">
+                        <h1 className="font-extrabold text-[32px] leading-[38px] md:text-xl mb-[16px]">
                             Переводы заграницу <br /> без препятствий
                         </h1>
-                        <p className="text-sm font-normal ">
+                        <p className="text-ls md:text-sm font-normal ">
                             Совершайте переводы в 123 страны и получайте деньги <br /> в  течение всего 2 часов с момента отправки
                         </p>
                         <p className="text-sm font-bold font-Roboto hidden md:block mt-[60px] mb-2">
@@ -21,6 +21,12 @@ const HomeBanner = () => {
                     </div>
                     <div className="col-span-3 md:mt-0 mt-[40px] ">
                         <HomeInputs></HomeInputs>
+                        <p className="text-ls font-bold text-center font-Roboto md:hidden block mt-[40px] mb-2">
+                            Excellent 4.8
+                            <span className="font-normal  font-Roboto mr-[13px]"> out of 5</span>
+                            <span><Image src='/images/shape.png' width={14} height={13} alt='star'></Image></span>
+                            <span className="ml-[4px] font-bold">Trustpilot</span>
+                        </p>
                     </div>
                 </div>
             </div>
